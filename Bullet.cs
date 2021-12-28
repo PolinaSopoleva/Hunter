fc7babc6b63aac3fc5e6050f1f8bd33385127d0e
RedBarboriska <65417508+RedBarboriska@users.noreply.github.com>
2021-12-29 01:37:38 +0200

    contrl player

---
+++ Bullet.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab9
+{
+    internal class Bullet : Collider
+    {
+        public Point playerPosition;
+        int x;
+        int y;
+        Image bullet;
+        Keys keyCode;
+        bool visible = false;
+        int playerWidth;
+        int playerHeight;
+        //String direction;
+        bool left;
+        bool right;
+        bool up;
+        bool down;
+        int speed = 5;
+        int clientSizeWidth;
+        int clientSizeHeight;
+        bool shoot;
+        int i = 1;
+
+
+        public Bullet(int playerWidth, int playerHeight, int clientSizeWidth, int clientSizeHeight)
+        {
+            //this.playerPosition = playerPosition;
+            bullet = Image.FromFile("..\\Resources\\bullet.png");
+            this.playerWidth = playerWidth;
+            this.playerHeight = playerHeight;
+            this.clientSizeWidth = clientSizeWidth;
+            this.clientSizeHeight = clientSizeHeight;
+        }
+
+        public Point getPosition()
+        {
+            return new Point(x, y);
+        }
+
+        private void toStart(Point playerPosition)
+        {
+            x = playerPosition.X + playerWidth / 2;
+            y = playerPosition.Y + playerHeight / 2;
+        }
+
+        public void changePosition(Graphics graphics, Point playerPosition, bool shoot)
+        {
+            if (!visible)
+            {
+                x = playerPosition.X + playerWidth / 2;
+                y = playerPosition.Y + playerHeight / 2;
+                if (shoot)
+                {
+                    if (keyCode == Keys.W)
+                    {
+                        //direction = "Up";
+                        up = true;
+                        fight(graphics, playerPosition);
+                        visible = true;
+                    }
+                    if (keyCode == Keys.A)
+                    {
+                        left = true;
+                        //direction = "Left";
+                        fight(graphics, playerPosition);
+                        visible = true;
+                    }
+                    if (keyCode == Keys.D)
+                    {
+                        right = true;
+                        //direction = "Right";
+                        fight(graphics, playerPosition);
+                        visible = true;
+                    }
+                    if (keyCode == Keys.S)
+                    {
+                        down = true;
+                        //direction = "Down";
+                        fight(graphics, playerPosition);
+                        visible = true;
+                    }
+                }
+            }
+            else
+            {
+                fight(graphics, playerPosition);
+            }
+
+        }
+        public void updatePosition(Keys keyCode)
+        {
+            this.keyCode = keyCode;
+
+        }
+        async Task PutTaskDelay()
+        {
+            await Task.Delay(1000);
+        }
+        public void reset()
+        {
+            this.keyCode = keyCode = Keys.None;
+
+        }
+
+        public void fight(Graphics graphics, Point playerPosition)
+        {
+            if (left)
+            {
+                if (x < -20)
+                {
+                    //i = 1;
+                    toStart(playerPosition);
+                    visible = false;
+                    left = false;
+                }
+                else
+                {
+                    x = x - i * speed;
+                    //i =i+ 1;
+                    graphics.DrawImage(bullet, x, y);
+                }
+            }
+            else if (right)
+            {
+                if (x > clientSizeWidth + 20)
+                {
+                    toStart(playerPosition);
+                    visible = false;
+                    right = false;
+                }
+                else
+                {
+                    x = x + 1 * speed;
+                    graphics.DrawImage(bullet, x, y);
+                }
+            }
+            else if (up)
+            {
+                if (y < -20)
+                {
+                    toStart(playerPosition);
+                    visible = false;
+                    up = false;
+                }
+                else
+                {
+                    y = y - 1 * speed;
+                    graphics.DrawImage(bullet, x, y);
+                }
+            }
+            else if (down)
+            {
+                if (y > clientSizeHeight + 20)
+                {
+                    toStart(playerPosition);
+                    visible = false;
+                    down = false;
+                }
+                else
+                {
+                    y = y + 1 * speed;
+                    graphics.DrawImage(bullet, x, y);
+                }
+            }
+        }
+
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Input;
+namespace Lab9
+{
+    class PlayerController
+    {
+        private int x = 10;
+        private int y = 10;
+        private int moveX = 0;
+        private int moveY = 0;
+        private int count = 0;
+        private int speed = 5;
+
+        private int width = 40;
+        private int height = 60;
+
+        private int directionV = 1;
+        private int directionH = 0;
+        private int clientSizeWidth;
+        private int clientSizeHeight;
+        private Keys keyCode;
+
+        public PlayerController(int clientSizeWidth, int clientSizeHeight)
+        {
+            this.clientSizeWidth = clientSizeWidth;
+            this.clientSizeHeight = clientSizeHeight;
+        }
+
+        public void reset()
+        {
+            keyCode = Keys.None;
+            moveX = 0;
+            moveY = 0;
+        }
+
+
+        public Point getPosition()
+        {
+            return new Point(x, y);
+        }
+        public Point getPlayerPosition()
+        {
+            if (keyCode == Keys.Left )
+            {
+                //moveX = x - 1 <= 0 ? 0 : -1;
+                moveY = 0;
+                if (x <= -20)
+                {
+                    x = clientSizeWidth + 20;
+                }
+                else
+                {
+                    moveX = -1;
+                }
+            }
+            if (keyCode == Keys.Right )
+            {
+                //moveX = x + 1 >= clientSizeWidth - 58 ? 0 : 1;
+                moveY = 0;
+                if (x >= clientSizeWidth)
+                {
+                    x = 0 - 20;
+                }
+                else
+                {
+                    moveX = 1;
+                }
+            }
+            if (keyCode == Keys.Up)
+            {
+                // moveY = y - 1 <= 0 ? 0 : -1;
+                moveX = 0;
+                if (y <= -40)
+                {
+                    y = clientSizeHeight + 40;
+                }
+                else
+                {
+                    moveY = -1;
+                }
+            }
+            if (keyCode == Keys.Down )
+            {
+                //moveY = y + 1 >= clientSizeHeight - 80 ? 0 : 1;
+                if (y >= clientSizeHeight)
+                {
+                    y = -40;
+                }
+                else
+                {
+                    moveY = 1;
+                }
+
+                // moveY = y + 1 >= clientSizeHeight - 80 ? 0 : 1;
+                moveX = 0;
+            }
+            x = x + moveX * speed;
+            y = y + moveY * speed;
+            return new Point(x, y);
+        }
+
+        public void updatePlayerPosition(Keys keyCode)
+        {
+            this.keyCode = keyCode;
+        }
+
+        private bool borderTest(Point player, int pwidth, int pheight)
+        {
+            Point center = new Point(player.X + pwidth / 2, player.Y + pheight / 2);
+            return center.X > 0
+                && center.Y > 0
+                && center.X < clientSizeWidth
+                && center.Y < clientSizeHeight;
+        }
+    }
+}
